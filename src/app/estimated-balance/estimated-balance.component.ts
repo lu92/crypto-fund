1b214feafcfe2867340b51cd4453231c07959c32
lu92 <biklucjan@gmail.com>
2020-09-27 21:00:12 +0200

    make estimated-balance component configurable

--- src/app/estimated-balance/estimated-balance.component.ts
+++ src/app/estimated-balance/estimated-balance.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-estimated-balance',
   templateUrl: './estimated-balance.component.html',
   styleUrls: ['./estimated-balance.component.css']
 })
-export class EstimatedBalanceComponent implements OnInit {
+export class EstimatedBalanceComponent {
 
-  constructor() { }
+  @Input('estimatedBtcBalance')
+  estimatedBtcBalance: number;
 
-  pnlValue = 5.25;
-  pnlChangePercentage = 0.71;
-  pnlMonthValue = -42.01;
-  pnlMonthChangePercentage = -6.53;
+  @Input('estimatedUsdBalance')
+  estimatedUsdBalance: number;
 
+  @Input('pnlValue')
+  pnlValue: number;
 
-  ngOnInit(): void {
-  }
+  @Input('pnlChangePercentage')
+  pnlChangePercentage: number;
+
+  @Input('pnlMonthValue')
+  pnlMonthValue: number;
+
+  @Input()
+  pnlMonthChangePercentage: number;
 
 }
